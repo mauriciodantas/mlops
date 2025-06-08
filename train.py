adb93d664d0ce1044ce7f552ea19b999f686fca2
Mauricio Dantas <maurcio@mdantas.net>
2025-06-08 10:46:45 -0300

    Fix pipelines

--- train.py
+++ train.py
@@ -54,9 +54,7 @@ def create_model(X):
 
 
 def config_mlflow():
-    os.environ['MLFLOW_TRACKING_USERNAME'] = 'renansantosmendes'
-    os.environ['MLFLOW_TRACKING_PASSWORD'] = '6d730ef4a90b1caf28fbb01e5748f0874fda6077'
-    mlflow.set_tracking_uri('https://dagshub.com/renansantosmendes/mlops-ead.mlflow')
+    mlflow.set_tracking_uri(os.environ.get('MLFLOW_TRACKING_URL'))
 
     mlflow.tensorflow.autolog(log_models=True,
                               log_input_examples=True,
